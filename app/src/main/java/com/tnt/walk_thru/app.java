90eccd8ade740a6f1110a758039f081c51a8f2e0
Thineshth <th.thinesh@gmail.com>
2016-01-23 20:47:36 -0500

    Adding Parse initialize

--- app/src/main/java/com/tnt/walk_thru/app.java
+++ app/src/main/java/com/tnt/walk_thru/app.java
@@ -4,13 +4,29 @@ import android.app.Application;
 import android.content.Context;
 
 import com.estimote.sdk.EstimoteSDK;
+import com.facebook.FacebookSdk;
+import com.parse.Parse;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseInstallation;
+import com.parse.ParseObject;
 
 public class app extends Application{
 
     private static app instance = new app();
 
-    String appId = "walk-thru-30b";
-    String appToken = "2c5ffc3c2fc7baf417473dcd2f7c0d81";
+    String appId = "secret";
+    String appToken = "secret";
+
+
+    /**
+     * Parse Application ID
+     */
+    private static String APPLICATION_ID = "secret";
+
+    /**
+     * Parse Client Key
+     */
+    private static String CLIENT_KEY = "secret";
 
     /**
      * default constructor that sets the instance
@@ -30,5 +46,16 @@ public class app extends Application{
         EstimoteSDK.initialize(this.getApplicationContext(), appId, appToken);
         // Optional, debug logging.
         EstimoteSDK.enableDebugLogging(true);
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, APPLICATION_ID, CLIENT_KEY);
+
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+
+        Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
+        ParseFacebookUtils.initialize(this);
+        FacebookSdk.sdkInitialize(this);
     }
 }
